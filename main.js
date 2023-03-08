f186774c16b0985f1b2780cb86943fd23d76a028
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 23:20:54 -0500

    feat (TypeOfMethods):toLowerCase()

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-/*El siguiente ejemplo define una función que divide una cadena en un array de cadenas usando el separador especificado. Después de la división de la cadena, la función muestra mensajes indicando la cadena original (antes de la división), el separador usado, el número de elementos del array y los elementos individuales del array.*/
+/*l método toLowerCase() devuelve el valor en minúsculas de la cadena que realiza la llamada.*/
 
-function dividirCadena(cadenaADividir,separador) {
-    var arrayDeCadenas = cadenaADividir.split(separador);
-    document.write('<p>La cadena original es: "' + cadenaADividir + '"');
-    document.write('<br>El separador es: "' + separador + '"');
-    document.write("<br>El array tiene " + arrayDeCadenas.length + " elementos: ");
- 
-    for (var i=0; i < arrayDeCadenas.length; i++) {
-       document.write(arrayDeCadenas[i] + " / ");
-    }
- }
- 
- var cadenaVerso = "Oh brave new world that has such people in it.";
- var cadenaMeses = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec";
- 
- var espacio = " ";
- var coma = ",";
- 
- dividirCadena(cadenaVerso, espacio);
- dividirCadena(cadenaVerso);
- dividirCadena(cadenaMeses, coma);
+let textoMayusculas="ALFABETO"
+document.write(textoMayusculas.toLowerCase())
