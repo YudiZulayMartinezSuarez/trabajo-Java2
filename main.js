f99873cd90f6c8cb7654a406413edef63e027053
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:03:36 -0500

    fead(TypeOfmethods): Length

---
+++ main.js
@@ -0,0 +1,8 @@
+/* La propiedad length de un objeto String representa la longitud de una cadena */
+
+let x = 'zulay';
+let empty = '';
+
+console.log('zulay ' + x.length + 'zulay tiene 7 unidades de código de longitud');
+
+console.log('La cadena vacía tiene una longitud de' + empty.length);
