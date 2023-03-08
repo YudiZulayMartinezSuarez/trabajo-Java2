813f81a7f50dae748e5ee86611ec57bfb997d68e
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:33:10 -0500

    FontFaceSet(typeOfMethods):lastIndexOf()

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-/*El método indexOf() devuelve el índice, dentro del objeto String que realiza la llamada, de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.*/
+/*El método lastIndexOf() devuelve la posicion (indice) en la que se encuentra el valorBusqueda, dentro del objeto String que realiza la llamada, de la última ocurrencia del valor especificado; o -1 si no se halla. La búsqueda se realiza empezando por el final de la cadena que realiza la llamada, empezando en indiceDesde.*/
 
-cuenta = 0;
-posicion = miCadena.indexOf("x");
-while ( posicion != -1 ) {
-   cuenta++;
-   posicion = miCadena.indexOf("x",posicion+1);
-}
+const animals = ['pajaro', 'conejo', 'oso', 'mariposa'];
 
+console.log(animals.lastIndexOf('oso'));
+
+console.log(animals.lastIndexOf('pajaro'));
