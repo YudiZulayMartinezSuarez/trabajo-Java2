d8fc45711a34228c96a8afad90d44a46340e8b5d
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:49:09 -0500

    FontFaceSet(typeOfMethods):matchAll(regexp)

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-/*El método localeCompare() retorna un número indicando si una cadena de carateres de referencia va antes, después o si es la misma que la cadena dada en orden alfabético.*/
+/*matchAll internamente hace un clon de la expresión regular, entonces a diferencia de regexp.exec, lastIndex no cambia a medida que la cadena es escaneada.*/
 
-function localeCompareLocales() {
-    try {
-      'foo'.localeCompare('bar', 'i');
-    } catch (e) {
-      return e.name === 'RangeError';
-    }
-    return false;
-  }
+const regexp = RegExp('[a-c]','g');
+regexp.lastIndex = 1;
+const cadena = 'abc';
+Array.from(cadena.matchAll(regexp), m => `${regexp.lastIndex} ${m[0]}`);
