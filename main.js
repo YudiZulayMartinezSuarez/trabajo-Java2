4c68854e47e44430333941e5fd80527c3b2c9df3
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:28:22 -0500

    FontFaceSet(typeOfMethods):concat()

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-/*El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.*/
+/*El método concat() combina dos o más cadenas de texto y devuelve una cadena de texto nueva.*/
 
-String.fromCharCode(numero1, numero2, ... numero_n);
+const str1 = 'zulay';
+const str2 = 'martinez';
+
+console.log(str1.concat(' ', str2));
+
+console.log(str2.concat(', ', str1));
+
