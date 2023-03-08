bd3441c33d797c3bd5de0e36574eb5246c3ec6ce
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 23:05:10 -0500

    feat(TypeOfMethods):replace(str|regex,newstr|func

--- main.js
+++ main.js
@@ -1,40 +1,4 @@
-/*Un entero entre 0 y +∞: [0, +∞), indicando el número de veces a repetir la cadena en la nueva cadenada creada que será devuelta.*/
-if (!String.prototype.repeat) {
-    String.prototype.repeat = function(count) {
-      'use strict';
-      if (this == null) {
-        throw new TypeError('can\'t convert ' + this + ' to object');
-      }
-      let str = '' + this;
-      count = +count;
-      if (count != count) {
-        count = 0;
-      }
-      if (count < 0) {
-        throw new RangeError('el recuento de repeticiones no debe ser negtivo');
-      }
-      if (count == Infinity) {
-        throw new RangeError('el recuento de repeticiones debe ser menor que nfinito');
-      }
-      count = Math.floor(count);
-      if (str.length == 0 || count == 0) {
-        return '';
-      }
-     
-      if (str.length * count >= 1 << 28) {
-        throw new RangeError('el recuento de repeticiones no debe desbordar el tamaño maximo de cadena')
-      }
-      let rpt = '';
-      for (;;) {
-        if ((count & 1) == 1) {
-          rpt += str;
-        }
-        count >>>= 1;
-        if (count == 0) {
-          break;
-        }
-        str += str;
-      }
-      return rpt;
-    }
-  }
+/*si queremos reemplazar en una cadena de texto, una subcadena por otra, se usa generalmente el método replace. Este método admite para buscar una subcadena lo mismo un String que una RegExp. Veamos a continuación cómo se usa este método:*/
+function str_replace($f, $r, $s){
+    return $s.replace(new RegExp("(" + (typeof($f) == "string" ? $f.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&") : $f.map(function(i){return i.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")}).join("|")) + ")", "g"), typeof($r) == "string" ? $r : typeof($f) == "string" ? $r[0] : function(i){ return $r[$f.indexOf(i)]});
+}
