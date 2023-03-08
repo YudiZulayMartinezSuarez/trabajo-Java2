565fdddb54ffb5df01cac242311b4ca9e9a9d067
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:22:55 -0500

    FontFaceSet(typeOfMethods): codePointAt()

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-/* charCodeAt devuelve NaN si el indice proporcionado no está entre 0 y 1 menos de la longitud de la cadena.
-El siguiente ejemplo devuelve 65, el valor Unicode para A.*/
+/* Índice del carácter en la cadena del que se quiere obtener el valor del Unicode code point.*/
 
+if (!String.prototype.codePointAt) {
+    (function() {
+      'use strict';
+      var codePointAt = function(position) {
+        if (this == null) {
+          throw TypeError();
+        }
+        var string = String(this);
+        var size = string.length;
+     
+        var index = position ? Number(position) : 0;
+        if (index != index) { 
+          index = 0;
+        }
 
-
-function myFunction() {
-    let str = "Hola Mundo";
-    let n = str.charCodeAt(0);
-    document.getElementById("demo").innerHTML = n;
-
-}
+        if (index < 0 || index >= size) {
+          return undefined;
+        }
+    
+        var first = string.charCodeAt(index);
+        var second;
+        if ( 
+          first >= 0xD800 && first <= 0xDBFF &&
+          size > index + 1 
+        ) {
+          second = string.charCodeAt(index + 1);
+          if (second >= 0xDC00 && second <= 0xDFFF) { 
+           
+            return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+          }
+        }
+        return first;
+      };
+      if (Object.defineProperty) {
+        Object.defineProperty(String.prototype, 'codePointAt', {
+          'value': codePointAt,
+          'configurable': true,
+          'writable': true
+        });
+      } else {
+        String.prototype.codePointAt = codePointAt;
+      }
+    }());
+  }
