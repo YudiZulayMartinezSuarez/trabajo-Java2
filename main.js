7506483d02134c3d18aecefbc6c9e2b5a5335f59
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 23:23:42 -0500

    feat (TypeOfMethods):trim()

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-/*El toUpperCase() método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.*/
+/*trim( ) elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).*/
 
-console.log( "alphabet".toUpperCase() );
+var orig = '   zul ';
+console.log(orig.trim());
