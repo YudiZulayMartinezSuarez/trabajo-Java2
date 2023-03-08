1c258e0db1784f743bf0c8b21fd33a2c4aff126e
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 22:17:05 -0500

    feat(TypeOfMethods): charCodeAt()

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-/* El siguiente ejemplo muestra caracteres de diferentes localizaciones en la cadena "Brave new world"*/
+/* charCodeAt devuelve NaN si el indice proporcionado no está entre 0 y 1 menos de la longitud de la cadena.
+El siguiente ejemplo devuelve 65, el valor Unicode para A.*/
 
-var cualquierCadena="Brave new world";
 
-console.log("El carácter en el índice 0 es '" + cualquierCadena.charAt(0) + "'")
-console.log("El carácter en el índice 1 es '" + cualquierCadena.charAt(1) + "'")
-console.log("El carácter en el índice 2 es '" + cualquierCadena.charAt(2) + "'")
-console.log("El carácter en el índice 3 es '" + cualquierCadena.charAt(3) + "'")
-console.log("El carácter en el índice 4 es '" + cualquierCadena.charAt(4) + "'")
-console.log("El carácter en el índice 999 es '" + cualquierCadena.charAt(999) + "'")
+
+function myFunction() {
+    let str = "Hola Mundo";
+    let n = str.charCodeAt(0);
+    document.getElementById("demo").innerHTML = n;
+
+}
