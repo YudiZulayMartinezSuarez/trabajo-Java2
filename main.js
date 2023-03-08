949387432a9bf4c798858cb7662de46d7ddc6032
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-07 23:08:26 -0500

    feat (TypeOfMethods):slice(in,end)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*si queremos reemplazar en una cadena de texto, una subcadena por otra, se usa generalmente el método replace. Este método admite para buscar una subcadena lo mismo un String que una RegExp. Veamos a continuación cómo se usa este método:*/
-function str_replace($f, $r, $s){
-    return $s.replace(new RegExp("(" + (typeof($f) == "string" ? $f.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&") : $f.map(function(i){return i.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")}).join("|")) + ")", "g"), typeof($r) == "string" ? $r : typeof($f) == "string" ? $r[0] : function(i){ return $r[$f.indexOf(i)]});
-}
+/*El método slice(in,end) extrae una sección de una cadena y devuelve una cadena nueva.*/
+
+let cadena1 = "La mañana se nos echa encima.";
+let cadena2 = cadena1.slice(3, -2);
+console.log(cadena2);
