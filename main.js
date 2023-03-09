be28f9f5a77f6d2669f18d0431b512535f183bb1
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-08 20:13:37 -0500

    feat(TypeOfMethods):logicos(NOT)

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 /*Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no booleanos, pueden devolver un valor no booleano*/
 
-comparacion = (diaHoy > diaAyer) || (diaHoy == diaAyer);
-	document.write("Resultado: " + comparacion + "<br>");
+{
+    result = !value;
+    alert(!true);   //El operador booleano NOT se representa con un signo de exclamación//
+    alert(!0);
+
+}
