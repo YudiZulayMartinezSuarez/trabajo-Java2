e17626717cd12a757e2316d0ab88bd283bdddf68
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-08 20:10:16 -0500

    feat(TypeOfMethods):logicos(OR)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 /*Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no booleanos, pueden devolver un valor no booleano*/
 
-comparacion = (diaHoy > diaAyer) && (mesHoy == mesAyer);
+comparacion = (diaHoy > diaAyer) || (diaHoy == diaAyer);
 	document.write("Resultado: " + comparacion + "<br>");
