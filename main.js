e3ea7003eda02426d338ce0726dc74e41d55862e
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-08 19:51:43 -0500

    feat(TypeOfMethods):comparacion(no igual)

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 /*Un operador de comparación compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos*/
 {
-Igual (==) //Devuelve true si los operandos son iguales.//
-
-3 == let1
-"3" == let1 
-3 == '3'
+ No es igual (!=) //Devuelve true si los operandos no son iguales.//
+    let != 4
+    let != "3"
 }
