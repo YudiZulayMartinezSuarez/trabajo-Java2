d820ca85e66b7a9376df0f881d57e9b7c64e4618
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-08 19:59:13 -0500

    feat(TypeOfMethods):comparacion( Mayor que )

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*Un operador de comparación compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos*/
 {
-    Desigualdad estricta (!==) //Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.//
-    let1 !== "3"
-    3 !== '3'
+    Mayor que (>)
+
+    let2 >  let1 20 > 2 //Devuelve true si el operando izquierdo es mayor que el operando derecho.//
+    
 }
