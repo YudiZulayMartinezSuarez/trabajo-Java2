754983253cfd41712da18821220063447662f132
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-08 19:53:12 -0500

    feat(TypeOfMethods):comparacion(estrictamente igual)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*Un operador de comparación compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos*/
 {
- No es igual (!=) //Devuelve true si los operandos no son iguales.//
-    let != 4
-    let != "3"
+    Estrictamente igual (===)  //Devuelve true si los operandos son iguales y del mismo tipo. //
+    
+    5=== let1
 }
