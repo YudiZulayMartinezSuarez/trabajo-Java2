9f499d816bdb3dd196a488404cd89206753776f6
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-11 17:52:53 -0500

    fead(TypeOfmethods):.max(x1, x2...)

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-/*La función Math.log() devuelve la base neutral de un número (base e)*/
+/*La función Math.max() retorna el mayor de cero o más números dados como parámetros de entrada, o NaN si cualquier parámetro no es un número y no puede ser convertido en uno.*/
 
-function base(x,y){
-    return Math.log(y) / Math.log(x);
-}
 
-console.log(base(2,8));
+console.log(Math.max(1,2,3));
+
+console.log(Math.max(-2,-3,-1));
+
+const array1 = [1,2,3];
+
+console.log(Math.max(array1));
