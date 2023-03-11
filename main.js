259bb2be97ccd1aac088bc422311d8adeb2e3e20
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-11 17:58:52 -0500

    fead(TypeOfmethods):.pow(base,exp)

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-/*El método estático Math.min() devuelve el menor de los números dados como parámetros de entrada, o Infinity si no hay parámetros.*/
+/*La función Math.pow() devuelve la base elevada al exponente , esto es, baseexponente.*/
 
-
-console.log(Math.min(1,2,3));
-
-console.log(Math.min(-2,-3,-1));
-
-const array1 = [1,2,3];
-
-console.log(Math.min(array1));
+Math.pow(7,2);
+Math.pow(5,8);
+Math.pow(10,2);
