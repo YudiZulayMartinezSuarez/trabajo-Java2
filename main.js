a556f745b818abbe4f3eeb69614beac32c61c973
YudiZulayMartinezSuarez <yzmartinez@uts.edu.co>
2023-03-11 17:42:51 -0500

    fead(TypeOfmethods): .abs(x)

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-/*Los operadores lógicos se utilizan normalmente con valores booleanos (lógicos); cuando lo son, devuelven un valor booleano. Sin embargo, los operadores && y || en realidad devuelven el valor de uno de los operandos especificados, por lo que si estos operadores se utilizan con valores no booleanos, pueden devolver un valor no booleano*/
+/*El método devuelve el valor absoluto de un número.Math.abs()*/
 
 {
-    result = !value;
-    alert(!true);   //El operador booleano NOT se representa con un signo de exclamación//
-    alert(!0);
+    let a = Math.abs(7.25);
+    let b = Math.abs(-7.25);
+    let c = Math.abs(null);
+    let d = Math.abs("hola");
+    let e = Math.abs(2-3);
 
 }
